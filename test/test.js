a3c950e2d48de715d67b8f00da93eebc5c88a759
Sebastien Ballesteros <sebastien.ballesteros@gmail.com>
2013-06-18 22:38:45 -0400

    adapted to new grammar

--- test/test.js
+++ test/test.js
@@ -25,7 +25,6 @@ describe('context', function(){
   it('should parse data asynchronosly', function(done){
     context.parseData(function(err){
       if(err) throw err;
-      fs.writeFileSync('s.json', JSON.stringify(context, null, 2));
       assert.deepEqual(context.context, require(path.join(root, 'expected', 'context.json')));
       done();
     });
@@ -33,6 +32,7 @@ describe('context', function(){
   
   it('should load data', function(done){
     context.load('data', function(err, data){
+      if(err) throw err;
       assert.deepEqual(data, require(path.join(root, 'expected', 'context.json')).data.source);
       done();
     });
@@ -40,6 +40,7 @@ describe('context', function(){
 
   it('should load metadata', function(done){
     context.load('mu_b', function(err, data){
+      if(err) throw err;
       assert.deepEqual(data, require(path.join(root, 'expected', 'context.json')).metadata.filter(function(x){return x.id==='mu_b'})[0].source);
       done();
     });
@@ -84,15 +85,10 @@ describe('model with remainder', function(){
 
 describe('model without remainder', function(){
 
-  var model
-    , pop_size;
+  var model;
 
-  before(function(done){
+  before(function(){
     model = new Model(require(path.join(root, 'context.json')), require(path.join(root, 'process_no_remainder.json')), require(path.join(root, 'link.json')), {rootContext:root});
-    model.load('N', function(err, my_pop_size){
-      pop_size = my_pop_size;
-      done();
-    });
   });
 
   it('should extract par_sv', function(){
@@ -101,18 +97,6 @@ describe('model without remainder', function(){
 
   it('should extract pop_size_eq_sum_sv', function(){
     assert(model.pop_size_eq_sum_sv);
-  })
-
-  it('should get the population size', function(done){
-    parse.obj_n(fs.createReadStream(path.join(root, 'results', 'hat_no_remainder_0.csv')), {key:'time', n:3}, function(err, hat_n){
-      if(err) throw err;      
-      assert.equal(hat_n.time, 3);
-      var city1__all = hat_n['S:city1__all'] + hat_n['I:city1__all'] + hat_n['R:city1__all'];
-      var city2__all = hat_n['S:city2__all'] + hat_n['I:city2__all'] + hat_n['R:city2__all'];
-            
-      assert.deepEqual(model._getPopSize_n(hat_n), {city1__all: city1__all, city2__all: city2__all});
-      done();
-    });      
   });
 
 });
@@ -381,7 +365,7 @@ describe('theta', function(){
     theta.mutate({
       set: ['r0:all:max:51', 'r0:all:guess:600']
     }, function(err){
-
+      if(err) throw err;
       assert.equal(theta.theta.parameter.r0.group.city1__all.guess.value, 51);
       assert.equal(theta.theta.parameter.r0.group.city2__all.guess.value, 51);
 
@@ -390,53 +374,50 @@ describe('theta', function(){
 
   });
 
-  it.skip('should plug hat', function(done){
-    var n = 3;
+  it('should plug hat', function(done){
+    var n = 21; //21 is the time
     theta.adapt();
     theta.plugHat({root:path.join(root, 'results'), state: 'hat_0.csv', index_state: n}, function(err){
       if(err) throw err;
       parse.obj_n(fs.createReadStream(path.join(root, 'results', 'hat_0.csv')), {key: 'time', n: n}, function(err, hat){
-        theta.load('N', function(err, N){          
-          assert.equal(theta.theta.parameter.S.group.city1__all.guess.value, hat['S:city1__all']/N[n+1][1]);
-          assert.equal(theta.theta.parameter.S.group.city2__all.guess.value, hat['S:city2__all']/N[n+1][2]);          
-          assert.equal(theta.theta.parameter.I.group.all.guess.value, (hat['I:city1__all']/N[n+1][1] + hat['I:city2__all']/N[n+1][2])/2);         
-          done();
-        });
+        var pop = theta._getPopSize_n(hat);
+        assert.equal(theta.theta.parameter.S.group.city1__all.guess.value, hat['S:city1__all']/ pop['city1__all']);
+        assert.equal(theta.theta.parameter.S.group.city2__all.guess.value, hat['S:city2__all']/ pop['city2__all']);          
+        assert.equal(theta.theta.parameter.I.group.all.guess.value, (hat['I:city1__all']/pop['city1__all'] + hat['I:city2__all']/pop['city2__all'])/2);         
+        done();
+
       });
     });
   });
 
-  it.skip('should plug hat with n === -1', function(done){
+  it('should plug hat with n === -1', function(done){
     var n = -1;
     theta.adapt();
     theta.plugHat({root:path.join(root, 'results'), state: 'hat_0.csv', index_state: n}, function(err){
       if(err) throw err;
       parse.obj_n(fs.createReadStream(path.join(root, 'results', 'hat_0.csv')), {key: 'time', n: n}, function(err, hat){
-        theta.load('N', function(err, N){          
-          assert.equal(theta.theta.parameter.S.group.city1__all.guess.value, hat['S:city1__all']/N[N.length-1][1]);
-          assert.equal(theta.theta.parameter.S.group.city2__all.guess.value, hat['S:city2__all']/N[N.length-1][2]);          
-          assert.equal(theta.theta.parameter.I.group.all.guess.value, (hat['I:city1__all']/N[N.length-1][1] + hat['I:city2__all']/N[N.length-1][2])/2);         
-          done();
-        });
+        var pop = theta._getPopSize_n(hat);
+        assert.equal(theta.theta.parameter.S.group.city1__all.guess.value, hat['S:city1__all']/pop['city1__all']);
+        assert.equal(theta.theta.parameter.S.group.city2__all.guess.value, hat['S:city2__all']/pop['city2__all']);          
+        assert.equal(theta.theta.parameter.I.group.all.guess.value, (hat['I:city1__all']/pop['city1__all'] + hat['I:city2__all']/pop['city2__all'])/2);         
+        done();
       });
     });
   });
 
-
-  it.skip('should plug hat and ungroup', function(done){
-    var n = 3;
+  it('should plug hat and ungroup', function(done){
+    var n = 21;
     theta.adapt();
     theta.plugHat({root:path.join(root, 'results'), state: 'hat_0.csv', index_state: n, ungroup: true}, function(err){
       if(err) throw err;
       parse.obj_n(fs.createReadStream(path.join(root, 'results', 'hat_0.csv')), {key: 'time', n: n}, function(err, hat){
-        theta.load('N', function(err, N){          
-          assert.equal(theta.theta.parameter.S.group.city1__all.guess.value, hat['S:city1__all']/N[n+1][1]);
-          assert.equal(theta.theta.parameter.S.group.city2__all.guess.value, hat['S:city2__all']/N[n+1][2]);          
-          
-          assert.equal(theta.theta.parameter.I.group.city1__all.guess.value, hat['I:city1__all']/N[n+1][1]);
-          assert.equal(theta.theta.parameter.I.group.city2__all.guess.value, hat['I:city2__all']/N[n+1][2]);
-          done();
-        });
+        var pop = theta._getPopSize_n(hat);
+        assert.equal(theta.theta.parameter.S.group.city1__all.guess.value, hat['S:city1__all']/pop['city1__all']);
+        assert.equal(theta.theta.parameter.S.group.city2__all.guess.value, hat['S:city2__all']/pop['city2__all']);          
+        
+        assert.equal(theta.theta.parameter.I.group.city1__all.guess.value, hat['I:city1__all']/pop['city1__all']);
+        assert.equal(theta.theta.parameter.I.group.city2__all.guess.value, hat['I:city2__all']/pop['city2__all']);
+        done();
       });
     });
   });
@@ -450,36 +431,37 @@ describe('theta', function(){
     theta.rescale({root:path.join(root, 'results'), rescale:['rep', 'hat_0.csv']}, function(err){
       if(err) throw err;
 
-      var hatMean = {
-        all__CDC__inc: 1091.3180,
-        all__google__inc:  1091.3180,
-        city2__CDC__inc: 546.4047,
-        city1__CDC__prev: 857.4636
-      };
-
       var dataMean = {
         all__CDC__inc: 1491.3415,
-        all__google__inc: 1240.3111,
+        all__google__inc:  1240.3111,
         city2__CDC__inc: 701.7872,
         city1__CDC__prev: 904.0426
+      };
+
+      var hatMean = {
+        all__CDC__inc: 1070.7256,
+        all__google__inc: 1070.7256,
+        city2__CDC__inc: 536.0949,
+        city1__CDC__prev: 841.3927
       };
             
       var goodRep = 0.0;
       for (var ts in hatMean){
         goodRep += dataMean[ts] / (hatMean[ts]/rep);
       }
-
+      
       assert.equal(theta.theta.parameter.rep.group.all.guess.value.toPrecision(4), (goodRep/4).toPrecision(4));
 
       done();
     });
   });
 
-
-  it.skip('should predict', function(done){
+  it('should predict', function(done){
 
     theta.adapt();
-    theta.predict(3, fs.createReadStream(path.join(root, 'results', 'X_0.csv')), fs.createReadStream(path.join(root, 'results', 'best_0.csv')), {}, function(err, thetas){
+    theta.predict(21, fs.createReadStream(path.join(root, 'results', 'X_0.csv')), fs.createReadStream(path.join(root, 'results', 'best_0.csv')), {}, function(err, thetas){
+      if(err) throw err;
+      assert.equal(thetas.length, 100);
       done();
     });
 
